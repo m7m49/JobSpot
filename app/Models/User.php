1f971d727d00c80c9dcf74309c30c096c18be2b5
m7m49 <mostafaissa512@gmail.com>
2024-05-20 17:56:23 +0300

    Adding JobSpot Project to GitHub

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Employer;
 
 class User extends Authenticatable
 {
@@ -43,5 +43,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function employer()
+    {
+        return $this->hasOne(Employer::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\TagController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [JobController::class, 'index']);
+
+Route::get('/search', SearchController::class);
+Route::get('/tags/{tag:name}', TagController::class);
+
+Route::middleware('auth')->group(function() {
+    Route::get('/myJobs', [JobController::class, 'myJobs']);
+    Route::get('/jobs/create', [JobController::class, 'create']);
+    Route::post('/jobs', [JobController::class, 'store']);
+    Route::delete('/logout', [SessionController::class, 'destroy']);
+});
+
+Route::middleware('guest')->group(function() {
+    Route::get('/register', [RegisteredUserController::class, 'create']);
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+
+    Route::get('/login', [SessionController::class, 'create']);
+    Route::post('/login', [SessionController::class, 'store']);
 });
